6f300bb79b625aa4f8d0da82bf967b01bf436df5
Selby Kendrick <selby.kendrick@gmail.com>
2016-03-19 18:37:43 -0500

    allow sorting on artists

--- app/routes/application/index.js
+++ app/routes/application/index.js
@@ -1,13 +1,20 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 export default Ember.Route.extend({
   setupController(controller, model) {
     controller.set('artists', model);
   },
-  model(params){
-    if(!params) {
+  model(params) {
+    if (!params) {
       params = {};
     }
+    _.defaults(params, {
+      sort_on: 'name',
+      sort_direction: 'asv',
+      per_page: 5,
+      page: 1
+    });
     return this.store.query('artist', params);
   }
 });
