7ef33fcd93fe809184a66c98382a4cdb49318cce
Selby Kendrick <selby.kendrick@gmail.com>
2016-03-20 19:52:19 -0500

    fix tests

--- tests/integration/components/audio-player-test.js
+++ tests/integration/components/audio-player-test.js
@@ -8,13 +8,12 @@ moduleForComponent('audio-player', 'Integration | Component | audio player', {
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{audio-player}}`);
 
-  assert.equal(this.$().text().trim(), '00:00/00:00');
+  assert.equal(this.$().text().indexOf('00:00/00:00') !== -1, true);
 
   // Template block usage:
   this.render(hbs `{{#audio-player}}test{{/audio-player}}`);
 
-  assert.equal(this.$().text().trim(), '00:00/00:00                      \ntest');
+  assert.equal(this.$().text().indexOf('00:00/00:00') !== -1, true);
 });
